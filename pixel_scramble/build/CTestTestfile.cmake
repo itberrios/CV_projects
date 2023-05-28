33973b30df9f944b20b3767d503adddaff3d0a08
blueeagle <itberrios6@gmail.com>
2023-05-28 12:27:52 -0500

    removed science projects

--- pixel_scramble/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/itber/Documents/learning/self_tutorials/CV_projects/pixel_scramble
-# Build directory: C:/Users/itber/Documents/learning/self_tutorials/CV_projects/pixel_scramble/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
