b63417fe7fd9f1475436a06bc571e35f6ec2d2bb
Isaac Berrios <60835780+itberrios@users.noreply.github.com>
2023-09-14 19:25:12 -0500

    Create raft_utils.py
    Added script to store helper functions to interface with RAFT class

---
+++ RAFT/raft_utils.py
@@ -0,0 +1,75 @@
+import numpy as np
+import torch
+from collections import OrderedDict
+from raft import RAFT
+from utils import flow_viz
+from utils.utils import InputPadder
+
+
+
+def process_img(img, device):
+    return torch.from_numpy(img).permute(2, 0, 1).float()[None].to(device)
+
+
+def load_model(weights_path, args):
+    model = RAFT(args)
+    pretrained_weights = torch.load(weights_path, map_location=torch.device("cpu"))
+    model = torch.nn.DataParallel(model)
+    model.load_state_dict(pretrained_weights)
+    model.to("cuda")
+    return model
+
+
+def inference(model, frame1, frame2, device, pad_mode='sintel',
+              iters=12, flow_init=None, upsample=True, test_mode=True):
+
+    model.eval()
+    with torch.no_grad():
+        # preprocess
+        frame1 = process_img(frame1, device)
+        frame2 = process_img(frame2, device)
+
+        padder = InputPadder(frame1.shape, mode=pad_mode)
+        frame1, frame2 = padder.pad(frame1, frame2)
+
+        # predict flow
+        if test_mode:
+          flow_low, flow_up = model(frame1,
+                                    frame2,
+                                    iters=iters,
+                                    flow_init=flow_init,
+                                    upsample=upsample,
+                                    test_mode=test_mode)
+          return flow_low, flow_up
+
+        else:
+            flow_iters = model(frame1,
+                               frame2,
+                               iters=iters,
+                               flow_init=flow_init,
+                               upsample=upsample,
+                               test_mode=test_mode)
+
+            return flow_iters
+
+
+def get_viz(flo):
+    flo = flo[0].permute(1,2,0).cpu().numpy()
+    return flow_viz.flow_to_image(flo)
+
+# sketchy class to pass to RAFT
+class Args():
+  def __init__(self, model='', path='', small=False, mixed_precision=True, alternate_corr=False):
+    self.model = model
+    self.path = path
+    self.small = small
+    self.mixed_precision = mixed_precision
+    self.alternate_corr = alternate_corr
+
+  """ Sketchy hack to pretend to iterate through the class objects """
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    raise StopIteration
+
